065deeb2268c9c5db40be3edbc6b66d51b7c7c27
Luis Miguel Jimenez <luismi86@gmail.com>
2024-06-05 21:51:25 -0500

    creando proyecto Spring boot

---
+++ src/main/java/grupo_app_servicios/appservicios/entidades/Usuario.java
@@ -0,0 +1,32 @@
+package grupo_app_servicios.appservicios.entidades;
+
+
+import grupo_app_servicios.appservicios.enumeraciones.Barrios;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name="uuid", strategy = "uuid2")
+    private String Id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String contrasena;
+    private String nombre;
+    private String apellido;
+    private Integer telefono;
+    @Enumerated(EnumType.STRING)
+    private Barrios barrios;
+    private boolean alta=true;
+
+}
