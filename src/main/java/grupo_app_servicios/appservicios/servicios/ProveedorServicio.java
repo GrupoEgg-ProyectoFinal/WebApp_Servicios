2ffdac3356ea8c198fb0331e365a853364e935e0
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-06-07 11:14:42 -0300

    implementación de mapeos, metodos de las querys y modificar

--- src/main/java/grupo_app_servicios/appservicios/servicios/ProveedorServicio.java
+++ src/main/java/grupo_app_servicios/appservicios/servicios/ProveedorServicio.java
@@ -6,12 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import grupo_app_servicios.appservicios.Dto.ImagenProveedorDTO;
 import grupo_app_servicios.appservicios.Dto.ProveedorDTO;
-import grupo_app_servicios.appservicios.entidades.ImagenProveedor;
 import grupo_app_servicios.appservicios.entidades.Proveedor;
 import grupo_app_servicios.appservicios.repositorios.ImagenProveedorRepositorio;
 import grupo_app_servicios.appservicios.repositorios.ProveedorRepositorio;
+import grupo_app_servicios.appservicios.utilidades.MapeadorDtoAEntidad;
+import grupo_app_servicios.appservicios.utilidades.MapeadorEntidadADto;
 
 @Service
 public class ProveedorServicio {
@@ -20,63 +20,62 @@ public class ProveedorServicio {
     @Autowired
     ImagenProveedorRepositorio imagenProveedorRepositorio;
 
-    private ProveedorDTO mapearEntidadADto(Proveedor proveedor) {
-        ProveedorDTO proveedorMapeado = new ProveedorDTO();
-        proveedorMapeado.setId(proveedor.getId());
-        proveedorMapeado.setNombre(proveedor.getNombre());
-        proveedorMapeado.setApellido(proveedor.getApellido());
-        proveedorMapeado.setEmail(proveedor.getEmail());
-        proveedorMapeado.setContrasena(proveedor.getContrasena());
+    private List<ProveedorDTO> mapearListaEntidadesADto(List<Proveedor> listaDeEntidades) {
+        // Se creó este método ya que se utiliza en otros métodos dentro de esta clase, 
+        // y así hacer más legible el código a través de la reutilización.
 
-        proveedorMapeado.setDescripcion(proveedor.getDescripcion());
-        proveedorMapeado.setTelefono(proveedor.getTelefono());
-        proveedorMapeado.setMatricula(proveedor.getMatricula());
-
-        if (proveedor.getFoto() != null) {
-            ImagenProveedor imagenProveedor = proveedor.getFoto();
-
-            ImagenProveedorDTO imagenProveedorDTO = new ImagenProveedorDTO();
-            // acá se mapearía un dto de imagenProveedor y se le asignaria al setter
-            proveedorMapeado.setFoto(imagenProveedorDTO); 
-        }
-
-        return proveedorMapeado;
-    }
-
-    private Proveedor mapearDtoAEntidad(ProveedorDTO proveedor) {
-        Proveedor proveedorMapeado = new Proveedor();
-        proveedorMapeado.setId(proveedor.getId());
-        proveedorMapeado.setNombre(proveedor.getNombre());
-        proveedorMapeado.setApellido(proveedor.getApellido());
-        proveedorMapeado.setEmail(proveedor.getEmail());
-        proveedorMapeado.setContrasena(proveedor.getContrasena());
-
-        proveedorMapeado.setDescripcion(proveedor.getDescripcion());
-        proveedorMapeado.setTelefono(proveedor.getTelefono());
-        proveedorMapeado.setMatricula(proveedor.getMatricula());
-
-        if (proveedor.getFoto() != null) {
-            ImagenProveedorDTO imagenProveedorDTO = proveedor.getFoto();
-
-            ImagenProveedor imagenProveedor = new ImagenProveedor();
-            // acá se mapearía una entidad de imagenProveedor y se le asignaria al setter
-            proveedorMapeado.setFoto(imagenProveedor); 
-        }
-
-        return proveedorMapeado;
+        // Con el .stream().map() y despúes el .toList() se crea una nueva lista y a cada 
+        // elemento se le aplica la funcion de mapeado que retorna el ProveedorDTO
+        return listaDeEntidades.stream().map(
+            proveedor -> MapeadorEntidadADto.mapearProveedor(proveedor)
+        ).toList();
     }
 
     @Transactional(readOnly = true)
     public List<ProveedorDTO> obtenerTodos() {
-        // Como proveedorRepositorio.findAll retorna las entidades de Proveedor, se crea una nueva lista en base a esa
-        // con el .stream().map() y a cada elemento le aplica la funcion que retorna el ProveedorDTO mapeado
-        return proveedorRepositorio.findAll().stream().map(
-            proveedor -> mapearEntidadADto(proveedor)
-        ).toList();
+        List<Proveedor> listaDeEntidades = proveedorRepositorio.findAll();
+        List<ProveedorDTO> listaDtosMapeados = mapearListaEntidadesADto(listaDeEntidades);
+
+        return listaDtosMapeados;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProveedorDTO> obtenerProveedoresSegunServicio(String nombreServicio) {
+        List<Proveedor> listaDeEntidades = proveedorRepositorio.obtenerProveedoresPorServicio(nombreServicio);
+        List<ProveedorDTO> listaDtosMapeados = mapearListaEntidadesADto(listaDeEntidades);
+
+        return listaDtosMapeados;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProveedorDTO> obtenerProveedoresSegunNombreYApellido(String valorBusqueda) {
+        List<Proveedor> listaDeEntidades = proveedorRepositorio.obtenerProveedoresPorNombreYOApellido(valorBusqueda);
+        List<ProveedorDTO> listaDtosMapeados = mapearListaEntidadesADto(listaDeEntidades);
+
+        return listaDtosMapeados;
     }
 
     @Transactional
     public void crear(ProveedorDTO proveedor) {
-        proveedorRepositorio.save(mapearDtoAEntidad(proveedor));
+        Proveedor dtoMapeadoAEntidad = MapeadorDtoAEntidad.mapearProveedor(proveedor);
+        proveedorRepositorio.save(dtoMapeadoAEntidad);
+    }
+
+    @Transactional
+    public void modificar(ProveedorDTO proveedor) {
+        // El .findById() retorna un Optional<Proveedor> pero al ponerle el .orElseThrow(), con la excepción
+        // que corresponda dentro, retornaría directamente la entidad del optional. En caso de que la búsqueda no
+        // dé resultado se arrojaría la excepción
+
+        // Una RuntimeException en pocas palabras es una excepción que puede ser lanzada durante la ejecución solamente, y no necesariamente
+        // frenaría la aplicación. La excepción común controla que el código esté bien y en caso de ser lanzada (ya sea en compilación o ejecución), 
+        // la aplicación se detendría hasta solucionar ese problema.
+        Proveedor dtoMapeadoAEntidad = proveedorRepositorio.findById(proveedor.getId()).orElseThrow(
+            () -> new RuntimeException("No se ha encontrado un proveedor con la id seleccionada")
+            // reemplazar la runtimeException por una excepcion personalizada en el futuro
+        );
+
+        //MapeadorDtoAEntidad.mapearProveedor(proveedor);
+        proveedorRepositorio.save(dtoMapeadoAEntidad);
     }
 }
