976ff3f10240f659e9417dd024469e1e1c743b05
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-06-11 23:10:04 -0300

    corrigiendo archivos

--- src/main/java/grupo_app_servicios/appservicios/controladorRest.java
+++ src/main/java/grupo_app_servicios/appservicios/controladorRest.java
@@ -1,14 +1,16 @@
 package grupo_app_servicios.appservicios;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
 public class controladorRest {
-    
 //llamar a la pagina principal
-public String pantallaInicial(){
-    return "index";
-       }
-
-
-
+    public String pantallaInicial(){
+        return "index";
+    }
 }
 
 
