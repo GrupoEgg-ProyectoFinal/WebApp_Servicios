c739380e0cc21e3558a7b5de986aa7cd85a9ec37
Federico Manuel Peche <fedepeche@hotmail.com>
2024-06-26 22:54:51 -0300

    act
    act

---
+++ src/main/resources/static/script/validarContraseña.js
@@ -0,0 +1,14 @@
+// FUNCION PARA VALIDAR CONTRASEÑAS EN LOS REGISTROS // 
+
+document.getElementById('formularioUsuario').addEventListener('submit', function(event) {
+    const contrasena = document.getElementById('contrasena').value;
+    const confirmarContrasena = document.getElementById('confirmarContrasena').value;
+    const mensajeError = document.getElementById('mensajeError');
+
+    if (contrasena !== confirmarContrasena) {
+        event.preventDefault(); // Evita el envío del formulario
+        mensajeError.textContent = "Las contraseñas no coinciden.";
+    } else {
+        mensajeError.textContent = "";
+    }
+});
